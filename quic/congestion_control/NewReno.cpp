e186e5623fd8c1aea95c96094d0bbc6ecd375a7f
Tim Fuchs <t.fuechsel@gmx.de>
2024-03-05 17:06:16 +0100

    mirror test

--- quic/congestion_control/NewReno.cpp
+++ quic/congestion_control/NewReno.cpp
@@ -54,7 +54,7 @@ void NewReno::onPacketSent(const OutstandingPacketWrapper& packet) {
 
 void NewReno::onAckEvent(const AckEvent& ack) {
   //   LOG(INFO) << "Acked " << ack.ackedPackets.size()
-  //             << " packets include: " << pack_nums;
+  //             << "  packets include: " << pack_nums;
 
   DCHECK(ack.largestNewlyAckedPacket.has_value() && !ack.ackedPackets.empty());
   subtractAndCheckUnderflow(conn_.lossState.inflightBytes, ack.ackedBytes);
